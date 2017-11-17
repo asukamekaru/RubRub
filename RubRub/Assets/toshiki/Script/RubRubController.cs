9bff00ca64fc8cf23dc529f550c76cb634507ced
KBC11605100\kbc11605100 <toshiki-o-16d@stu.kbc.ac.jp>
2017-11-17 13:12:07 +0900

    panel追加

---
+++ RubRub/Assets/toshiki/Script/RubRubController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RubRubController : MonoBehaviour {
+
+    private float OldMousePositionX;
+    private float OldMousePositionY;
+
+    private float OldMousePositionX;
+    private float OldMousePositionY;
+
+    // Use this for initialization
+    void Start () {
+        Vector3 mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y,
+          Input.mousePosition.z);
+        OldMousePositionX = mousePosition.x;
+        OldMousePositionY = mousePosition.y;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        //マウスポジション取得
+        Vector3 mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y,
+            Input.mousePosition.z);
+
+
+	}
+}
