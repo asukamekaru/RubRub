2e117963667b51d5793fe7f0b1296f193438b2f5
KBC11606120\kbc11606120 <asuka-m-16d@stu.kbc.ac.jp>
2017-11-22 15:56:29 +0900

    競合解決の為の

---
+++ RubRub/Assets/hikaru/Player_Move2.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Move2 : MonoBehaviour {
+
+    //------------------------------
+    [SerializeField]
+    private float movement = 150f;
+    [SerializeField]
+    private float rotateSpeed = 5f;
+    float moveX = 0f, moveZ = 0f;
+    Rigidbody rb;
+    //------------------------------
+
+    //------------------------------
+    Animator WalkAnimator;
+    //------------------------------
+
+	// Use this for initialization
+	void Start () {
+
+        //角度
+        rb = this.GetComponent<Rigidbody>();
+
+        //
+        this.WalkAnimator = GetComponent<Animator>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //プレイヤー移動、角度調整
+        if (Input.GetAxis("Horizontal") < 0)
+        {
+            moveX = Input.GetAxis("Horizontal") * Time.deltaTime * movement;
+            WalkAnime(true);
+        }
+        else if (Input.GetAxis("Horizontal") > 0)
+        {
+            moveX = Input.GetAxis("Horizontal") * Time.deltaTime * movement;
+            WalkAnime(true);
+        }
+        else if (Input.GetAxis("Vertical") > 0)
+        {
+            moveZ = Input.GetAxis("Vertical") * Time.deltaTime * movement;
+            WalkAnime(true);
+        }
+        else if (Input.GetAxis("Vertical") < 0)
+        {
+            moveZ = Input.GetAxis("Vertical") * Time.deltaTime * movement;
+            WalkAnime(true);
+        }
+        else
+        {
+            WalkAnime(false);
+        }
+
+
+
+        Vector3 direction = new Vector3(moveX, 0, moveZ);
+        if (direction.magnitude > 0.01f)
+        {
+            float step = rotateSpeed * Time.deltaTime;
+            Quaternion myQ = Quaternion.LookRotation(direction);
+            this.transform.rotation = Quaternion.Lerp(transform.rotation, myQ, step);
+        }
+
+        //アニメーション管理
+        
+    }
+
+    void WalkAnime(bool bChange)
+    {
+        WalkAnimator.SetBool("Walk", bChange);
+    }
+
+    void FixedUpdate()
+    {
+        rb.velocity = new Vector3(moveX, 0, moveZ);
+    }
+
+}
