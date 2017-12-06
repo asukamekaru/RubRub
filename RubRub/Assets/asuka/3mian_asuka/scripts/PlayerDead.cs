02c389b321899b08131a69e7bd533c47bbebe855
KBC11606120\kbc11606120 <asuka-m-16d@stu.kbc.ac.jp>
2017-12-06 12:58:53 +0900

    [fix]敵キャラに触れると死ぬスクリプト（アニメーションのみ）

---
+++ RubRub/Assets/asuka/3mian_asuka/scripts/PlayerDead.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDead : MonoBehaviour {
+
+    Animator _animator;
+
+	// Use this for initialization
+	void Start () {
+        this._animator = GetComponent<Animator>();
+	}
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Enemy") _animator.SetBool("Dead", true);
+    } 
+}
