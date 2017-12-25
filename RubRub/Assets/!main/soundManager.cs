f4a3838464ad004b54720e0a384402f29e517ab5
asukamekaru <asuka-m-16d@stu.kbc.ac.jp>
2017-12-25 15:59:06 +0900

    ホームシーンの仮組み

---
+++ RubRub/Assets/!main/soundManager.cs
@@ -0,0 +1,74 @@
+﻿//=================================================
+// 音を管理するスクリプト
+// AsukaMekaru
+// 2017/12/25
+//=================================================
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+
+[System.Serializable]
+public class AudioSet
+{
+    public AudioClip seClip;
+    public float volume;
+}
+
+public class soundManager : MonoBehaviour
+{
+    #region //シングルトン
+    public static soundManager Instance{ get; private set; }
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(this);
+        }
+        else
+        {
+            DestroyImmediate(this);
+        }
+    }
+    #endregion
+    [SerializeField]
+    public List<AudioSet> seList = new List<AudioSet>();
+    public AudioSource bgmSource;//BGM's
+    public AudioSource seSource;//SE's
+    public AudioClip[] bgmClip;
+
+    //音を再生
+    public void PlaySound(int seNum, bool isPlaying)
+    {
+        if (isPlaying)
+        {
+            //seSource.volume =  seList[seNum].volume;
+            if (!seSource.isPlaying)
+            {
+                seSource.PlayOneShot(seList[seNum].seClip, seList[seNum].volume);
+            }
+        }
+        else
+        {
+            seSource.PlayOneShot(seList[seNum].seClip, seList[seNum].volume);
+        }
+    }
+
+    //音を変える
+    public void ChangeBgm(int bgmNum)
+    {
+        if (!bgmSource.isPlaying)
+        {
+            bgmSource.clip = bgmClip[bgmNum];
+            bgmSource.Play();
+        }
+    }
+
+    //音を止める
+    public void StopBgm()
+    {
+        bgmSource.Stop();
+    }
+}
---
+++ RubRub/Assets/asuka/2home_asuka/scripts/selectUIScript.cs
@@ -0,0 +1,22 @@
+﻿//=================================================
+// GameHomeScene <= ステージセレクトボタンのスクリプト
+// AsukaMekaru
+// 2017/11/23
+//=================================================
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class selectUIScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
