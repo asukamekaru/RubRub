1571c39ffeabe27f8aeddf54933fcc8fedcf3fcf
KBC11605100\kbc11605100 <toshiki-o-16d@stu.kbc.ac.jp>
2018-01-09 12:24:26 +0900

    タスク途中

---
+++ RubRub/Assets/toshiki/3main_toshiki/scene/GameClear/CameraMove.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour
+{
+    //回転の変数
+    public float MoveTotalRotation = 0.0f;
+    float MoveRotationSpeed = 4.0f;
+    float MaxRotation = 360.0f;
+    //接近するタイミング
+    float MovePoint = 0.0f;
+    //接近の変数
+    float MovePositionMaxY = -1.0f;
+    float MovePositionMaxZ = 3.0f;
+    float MovePositionYSpeed = 0.01f;
+    float MovePositionZSpeed = 0.03f;
+    float CameraNowPositionY = 0.0f;
+    float CameraNowPositionZ = 0.0f;
+    void Update()
+    {
+        //回転
+        if (MoveTotalRotation < MaxRotation)
+        {
+            MoveTotalRotation += MoveRotationSpeed;
+            transform.Rotate(new Vector3(0.0f, MoveRotationSpeed, 0.0f));
+
+            if (MoveTotalRotation >= MaxRotation) MoveTotalRotation = MaxRotation;
+        }
+        //接近
+        if(MoveTotalRotation >= MovePoint)
+        {
+            if(CameraNowPositionY > MovePositionMaxY)
+            {
+                CameraNowPositionY -= MovePositionYSpeed;
+            }
+            if(CameraNowPositionZ < MovePositionMaxZ)
+            {
+                CameraNowPositionZ += MovePositionZSpeed;
+            }
+            transform.position = new Vector3(0.0f, CameraNowPositionY, CameraNowPositionZ);
+        }
+       
+    }
+}
---
+++ RubRub/Assets/toshiki/EnemyWarp.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyWarp : WallBase
+{ 
+    string TagName = "Enemy";
+    string WallTagName = "EnemyWall";
+    bool EnemyCollisionFlg;
+    GameObject Enemy = null;
+
+    void Update()
+    {
+        if(EnemyCollisionFlg)
+        {
+            //if (NearObjectRetrieval(this.gameObject, TagName[WallType - 1]))//TagNameの要素が２つしかないため　WallType - 1
+            //{
+            //    NearTriggerObject = WallType;
+            //}
+            //EnemyWarp(this.gameObject, WallName[WallType - 1], NearTriggerObject);
+        }
+    }
+
+   
+    //オブジェクトが衝突したとき
+    public void OnCollisionEnter(Collision collision)
+    {
+        if (this.tag == "EnemyWall")
+        {
+            if (collision.gameObject.tag == "Enemy")
+            {
+                EnemyCollisionFlg = true;
+            }
+        }
+
+    }
+}
