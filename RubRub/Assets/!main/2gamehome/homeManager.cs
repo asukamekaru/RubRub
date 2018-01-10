cb3e3a7d61e0e78e585b5e2850acc8ad49e5157a
asukamekaru <asuka-m-16d@stu.kbc.ac.jp>
2018-01-11 01:20:01 +0900

    home

--- RubRub/Assets/!main/2gamehome/homeManager.cs
+++ RubRub/Assets/!main/2gamehome/homeManager.cs
@@ -1,4 +1,9 @@
-﻿using System.Collections;
+﻿//=================================================
+// ホーム画面を管理するスクリプト
+// AsukaMekaru
+// 2017/12/25
+//=================================================
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -6,16 +11,18 @@ using UnityEngine.UI;
 public class homeManager : MonoBehaviour
 {
     ////////////////////////////////////// 変数シンボル //////////////////////////////////////
+    const int MAXSTAGE = 3; //最大ステージ
+
     selectUIScript selectUiScript;
-
-    readonly int MAXSTAGE = 3;
-
-    [SerializeField]
+    
     [Header("ステージを選ぶボタン")]
-    private List<GameObject> Stagebtn = new List<GameObject>();//ステージ選択のボタンのリスト
+    public GameObject[] Stagebtn = new GameObject[MAXSTAGE];
 
-    [Header("デフォルトの中央ボタンの位置")]
-    public Vector3 DefCenterButtonPosi;//x0 y-120 z0 ボタンの中央の位置
+    [Header("中央ボタンの位置")]
+    public Vector3 CenterButtonPosi;//x0 y-120 z0 ボタンの中央の位置
+    [Header("中央以外のボタンの位置")]
+    public Vector3 OtherButtonPosi;//x0 y-120 z0 ボタンの中央の位置
+
 
     ////////////////////////////////////// 変数 //////////////////////////////////////
     private int iNowSelectStage = 0;//ボタンを押したら飛ばされるステージの番号
@@ -25,8 +32,7 @@ public class homeManager : MonoBehaviour
     {
         for (int i = 0; i < MAXSTAGE; i++)
         {
-            Stagebtn[i].gameObject.GetComponent<selectUIScript>().getMyNum(i);//飛ばされるステージ番号の初期化
-            Stagebtn[i].gameObject.GetComponent<Button>().onClick.AddListener(BTN);//ボタンをクリックしたときの処理
+            Stagebtn[i].gameObject.GetComponent<selectUIScript>().getMyNum(iNowSelectStage - i);//飛ばされるステージ番号の初期化
         }
 
     }
@@ -37,25 +43,17 @@ public class homeManager : MonoBehaviour
 
     }
 
-    void BTN() { Debug.Log("a"); }
-
     public void getControll(string s)
     {
-        if (s == "right")
-        {
-            ++iNowSelectStage;//右ボタンを押し、かつ最大ステージでなければ増やす
-        }
+        if (s == "right") ++iNowSelectStage;//右ボタンを押し、かつ最大ステージでなければ増やす
 
-        if (s == "left")
-        {
-            --iNowSelectStage;//左ボタンを押し、かつ最大ステージでなければ減らす
-        }
+        if (s == "left" ) --iNowSelectStage;//左ボタンを押し、かつ最大ステージでなければ減らす
 
         Debug.Log(iNowSelectStage);
 
         for (int i = 0; i < MAXSTAGE; i++)
         {
-            Stagebtn[i].gameObject.GetComponent<selectUIScript>().getMyNum(iNowSelectStage + i);
+            Stagebtn[i].gameObject.GetComponent<selectUIScript>().getMyNum(iNowSelectStage - i);
         }
     }
 }
--- RubRub/Assets/asuka/2home_asuka/scripts/selectUIScript.cs
+++ RubRub/Assets/asuka/2home_asuka/scripts/selectUIScript.cs
@@ -7,29 +7,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class selectUIScript : MonoBehaviour
 {
     [SerializeField]
     private homeManager homemanager;
 
-    int iMyNowNum;
+    bool bBtnAnime;
+
+    Vector3 setvec;
 
     // Use this for initialization
     void Start()
     {
-
+        this.gameObject.GetComponent<Button>().onClick.AddListener(ClickBtn);
     }
 
     // Update is called once per frame
     void Update()
     {
-        this.transform.localPosition = 
-            new Vector3(homemanager.DefCenterButtonPosi.x - iMyNowNum * -250, homemanager.DefCenterButtonPosi.y, homemanager.DefCenterButtonPosi.z);
+        this.transform.localPosition = setvec;
     }
 
-    public void getMyNum(int i)
+    private void ClickBtn() { }//ボタンがクリックされたときの処理
+
+    public void getMyNum(int get)//自分の位置を取得
     {
-        iMyNowNum = i;
+        setvec.x = (get == 0) ? homemanager.CenterButtonPosi.x : homemanager.OtherButtonPosi.x - (get * homemanager.OtherButtonPosi.x);
+        setvec.y = (get == 0) ? homemanager.CenterButtonPosi.y : homemanager.OtherButtonPosi.y;
+        setvec.z = (get == 0) ? homemanager.CenterButtonPosi.z : homemanager.OtherButtonPosi.z;
     }
 }
