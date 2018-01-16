c0fc53c0998c76facbf83e0e0c5115063364dee8
KBC11606120\kbc11606120 <asuka-m-16d@stu.kbc.ac.jp>
2018-01-16 13:59:36 +0900

    結合70%

--- RubRub/Assets/asuka/3mian_asuka/scripts/wallclear.cs
+++ RubRub/Assets/asuka/3mian_asuka/scripts/wallclear.cs
@@ -1,34 +1,50 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class wallclear : MonoBehaviour {
     ////////////////////////////////////// 変数シンボル //////////////////////////////////////
-
     
-    [Header("検知する範囲")]
-    [SerializeField]
-    private float fWallRange;
+    private float fWallRangeX = 0.5f;
+    private float fWallRangeZ = 2.0f;
 
     [Header("プレイヤーオブジェクト")]
     [SerializeField]
     private GameObject Player;
 
+    [Header("透明、非透明のイメージ")]
+    [SerializeField]
+    public Material[] _material;
     ////////////////////////////////////// 変数 //////////////////////////////////////
 
-    float fWallXMin,fWallXMax;
+    float fWallXMin, fWallXMax, fWallZMin,fWallZMax;
+
+    Image image;
 
 	// Use this for initialization
 	void Start () {
-        fWallXMin = this.transform.localScale.x / 2;
-        fWallXMin = fWallXMin * 3;
+        fWallXMin = this.transform.localPosition.x - (this.transform.localScale.x / 2 - fWallRangeX);
+        fWallXMax = this.transform.localPosition.x + (this.transform.localScale.x / 2 + fWallRangeX);
+        fWallZMin = this.transform.localPosition.z;
+        fWallZMax = this.transform.localPosition.z + fWallRangeZ;
+        
+
+        this.GetComponent<Renderer>().material = _material[0];
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        if (Player.transform.position.x > fWallXMin && Player.transform.position.x < fWallXMin)
-        {
 
+        //範囲内なら透明になる
+        if (Player.transform.localPosition.x > fWallXMin && Player.transform.localPosition.x < fWallXMax && Player.transform.localPosition.z < fWallZMax && Player.transform.localPosition.z > fWallZMin)
+        {
+                this.GetComponent<Renderer>().material = _material[0];
+        }
+        //範囲外なら普通になる
+        else
+        {
+            this.GetComponent<Renderer>().material = _material[1];
         }
 	}
 }
