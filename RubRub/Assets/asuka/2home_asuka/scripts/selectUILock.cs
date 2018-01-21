525cc9fa4d77e46ce0aa89c46cd119db9aed7db5
asukamekaru <asuka-m-16d@stu.kbc.ac.jp>
2018-01-22 00:27:54 +0900

    ホーム仮

---
+++ RubRub/Assets/asuka/2home_asuka/scripts/selectUILock.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class selectUILock : MonoBehaviour {
+
+    ////////////////////////////////////// 変数シンボル //////////////////////////////////////
+    [Header("親のスイッチ")]
+    [SerializeField]
+    private selectUIScript parentUI;
+
+    [Header("ロック画像")]
+    [SerializeField]
+    public Sprite[] _sprite;
+    
+
+    ////////////////////////////////////// 変数 //////////////////////////////////////
+
+    // Use this for initialization
+    void Start () {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+        this.transform.GetComponent<RectTransform>().sizeDelta = new Vector2(parentUI.mySize, parentUI.mySize);
+
+        this.gameObject.GetComponent<Image>().sprite = _sprite [1];
+    }
+}
--- RubRub/Assets/asuka/2home_asuka/scripts/selectUIScript.cs
+++ RubRub/Assets/asuka/2home_asuka/scripts/selectUIScript.cs
@@ -19,6 +19,8 @@ public class selectUIScript : MonoBehaviour
     ////////////////////////////////////// 変数 //////////////////////////////////////
     //bool bBtnAnime;
 
+    public Vector3 myVec;//自分の座標
+    public float mySize;//自分のサイズ
     private Vector3 getVec;//取ってきた位置を保存するもの
     private float getSize;//取ってきたサイズを保存するもの
 
@@ -31,13 +33,55 @@ public class selectUIScript : MonoBehaviour
     // Use this for initialization
     void Start()
     {
+        mySize = this.transform.GetComponent<RectTransform>().sizeDelta.x;
+        myVec = this.transform.localPosition;
         this.gameObject.GetComponent<Button>().onClick.AddListener(ClickBtn);
     }
 
     // Update is called once per frame
     void Update()
     {
+        //ボタン移動のアニメーション
 
+        //位置X
+        if (myVec.x >= getVec.x)
+        {
+            myVec.x -= homemanager.ButtonScrollSpeed;
+            if (myVec.x <= getVec.x) myVec.x = getVec.x;
+        }
+        else if (myVec.x <= getVec.x)
+        {
+            myVec.x += homemanager.ButtonScrollSpeed;
+            if (myVec.x >= getVec.x) myVec.x = getVec.x;
+        }
+
+        //位置Z
+        if (myVec.z >= getVec.z)
+        {
+            myVec.z -= homemanager.ButtonScrollSpeed;
+            if (myVec.z <= getVec.z) myVec.z = getVec.z;
+        }
+        else if (myVec.z <= getVec.z)
+        {
+            myVec.z += homemanager.ButtonScrollSpeed;
+            if (myVec.z >= getVec.z) myVec.z = getVec.z;
+        }
+
+        //サイズ
+        if (mySize >= getSize)
+        {
+            mySize -= homemanager.ButtonScrollSpeed;
+            if (mySize <= getSize) mySize = getSize;
+        }
+        else if (mySize <= getSize)
+        {
+            mySize += homemanager.ButtonScrollSpeed;
+            if (mySize >= getSize) mySize = getSize;
+        }
+
+        //自分に当てる
+        this.transform.localPosition = myVec;
+        this.transform.GetComponent<RectTransform>().sizeDelta = new Vector2(mySize, mySize);
     }
 
     public void getMyNum(int get)//自分の位置を取得
@@ -48,11 +92,8 @@ public class selectUIScript : MonoBehaviour
         getVec.z = (get == 0) ? homemanager.CenterButtonPosi.z : changeSign((get * homemanager.OtherButtonPosi.z));
 
         //サイズを保存
-        this.transform.GetComponent<RectTransform>().sizeDelta = (get == 0) ?
-            new Vector2(homemanager.CenterButtonSize, homemanager.CenterButtonSize) :
-            new Vector2(homemanager.OtherButtonSize, homemanager.OtherButtonSize);
+        getSize = (get == 0) ? homemanager.CenterButtonSize : homemanager.OtherButtonSize;
 
-        this.transform.localPosition = getVec;
     }
 
     private void ClickBtn() { if (nextScene == "NULL") return; SceneManager.LoadScene(nextScene); }//ボタンがクリックされたときの処理
