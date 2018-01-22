fb2f97404cfbf3b2b66526b589c08beeef3099d4
asukamekaru <asuka-m-16d@stu.kbc.ac.jp>
2018-01-23 00:52:55 +0900

    チュートリアル仮組み

---
+++ RubRub/Assets/asuka/3mian_asuka/scripts/TutorialMgr.cs
@@ -0,0 +1,60 @@
+﻿//=================================================
+// GameMainScene <= チュートリアルマネージャー
+// AsukaMekaru
+// 2017/11/23
+//=================================================
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TutorialMgr : MonoBehaviour {
+
+    ////////////////////////////////////// 変数シンボル //////////////////////////////////////
+    const int MAXHINT = 6;
+
+    [Header("各ポイントのオブジェクト")]
+    [SerializeField]
+    private GameObject[] _HintObject = new GameObject[MAXHINT];
+    [Header("各ポイントのヒント画像")]
+    public Sprite[] _HintImage = new Sprite[MAXHINT];
+    [Header("各ポイントのパネル")]
+    [SerializeField]
+    private GameObject TutorialPanel;
+
+    ////////////////////////////////////// 変数 //////////////////////////////////////
+    [HideInInspector]
+    public int iNowHint;//今のヒント番号
+
+    // Use this for initialization
+    void Start () {
+        //最初のやつだけアクティブにする
+        _HintObject[0].gameObject.SetActive(true);
+        //それ以外を非アクティブにする
+        for(int i = 1;i < MAXHINT;i++) _HintObject[i].gameObject.SetActive(false);
+
+    }
+
+    //範囲に入ればチュートリアルの説明パネルをアクティブにして時間を止める
+    public void active(){ TutorialPanel.gameObject.SetActive(true); Time.timeScale = 0; }
+    public void inactive() {
+        //最大値以下なら次のヒントへ移る
+        if (iNowHint < MAXHINT) iNowHint++;
+        //チュートリアルパネルを非アクティブにする
+        TutorialPanel.gameObject.SetActive(false);
+        //今表示されているヒントオブジェクトを非アクティブにする
+        _HintObject[iNowHint - 1].gameObject.SetActive(false);
+        //最大じゃなければ次のヒントオブジェクトをアクティブにする
+        if (iNowHint != MAXHINT)
+        {
+            _HintObject[iNowHint].gameObject.SetActive(true);
+        }
+        //最大値ならすべてのヒントオブジェクトを非アクティブにする
+        else
+        {
+            _HintObject[iNowHint - 1].gameObject.SetActive(false);
+        }
+        //時間をすすめる
+        Time.timeScale = 1;
+    }
+}
---
+++ RubRub/Assets/asuka/3mian_asuka/scripts/TutorialPanel.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TutorialPanel : MonoBehaviour {
+
+    ////////////////////////////////////// 変数シンボル //////////////////////////////////////
+
+    [Header("デフォルトのポジション")]
+    [SerializeField]
+    private float fDefPosition;
+    [Header("移動速度")]
+    [SerializeField]
+    private float fMoveSpeed;
+
+    [Header("チュートリアルマネージャー")]
+    [SerializeField]
+    TutorialMgr tutorialmgr;
+    ////////////////////////////////////// 変数 //////////////////////////////////////
+    bool bReturn = false;
+
+    // Use this for initialization
+    void Start () {
+        this.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!bReturn)Up();
+
+        this.gameObject.GetComponent<Image>().sprite = tutorialmgr._HintImage[tutorialmgr.iNowHint];
+
+        if (Input.GetMouseButtonDown(0)) bReturn = true;//タップしたら一旦パネルを戻す
+
+        if (bReturn && Down()) { tutorialmgr.inactive(); bReturn = false; }//戻れば終了
+    }
+
+    void Up()
+    {
+        if (this.gameObject.transform.localPosition.y > 0)//下に移動
+        {
+            this.gameObject.transform.localPosition -= new Vector3(0, fMoveSpeed, 0);
+        }
+
+        if (this.gameObject.transform.localPosition.y < 0)//過ぎたら修正
+        {
+            this.gameObject.transform.localPosition = new Vector3(0, 0, 0);
+        }
+    }
+
+    bool Down()
+    {
+        if (this.gameObject.transform.localPosition.y < fDefPosition)//上に移動
+        {
+            this.gameObject.transform.localPosition += new Vector3(0, fMoveSpeed, 0);
+        }
+
+        if (this.gameObject.transform.localPosition.y > fDefPosition)//過ぎたら修正
+        {
+            this.gameObject.transform.localPosition = new Vector3(0, fDefPosition, 0);
+        }
+
+        if (this.gameObject.transform.localPosition.y == fDefPosition)
+        {
+            return true;//終わり
+        }
+
+        return false;
+    }
+}
