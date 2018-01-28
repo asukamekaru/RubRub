a8fd3252082402f28250a98179f5c82c9debb4ba
asukamekaru <asuka-m-16d@stu.kbc.ac.jp>
2018-01-29 02:16:36 +0900

    スタート・ゴールUI

---
+++ RubRub/Assets/asuka/3mian_asuka/scripts/StartEndUIScript.cs
@@ -0,0 +1,121 @@
+﻿//=================================================
+// GameMainScene <= スタート、エンド時に表示されるUIを管理するスクリプト
+// AsukaMekaru
+// 2018/01/29
+//=================================================
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StartEndUIScript : MonoBehaviour
+{
+
+    ////////////////////////////////////// 変数シンボル //////////////////////////////////////
+    private enum SEUI_STATUS { _PANEL_START_, _PANEL_SCALEUP_, _PANEL_FIRST_INTERVAL_, _PANEL_SCALEDOWN_, _PANEL_LAST_INTERVAL_ };//ゲームの状態
+    private SEUI_STATUS seuistatus;
+
+    [Header("UI画像START")]
+    [SerializeField]
+    private Sprite START;
+    [Header("UI画像GOAL")]
+    [SerializeField]
+    private Sprite GOAL;
+    [Header("UI画像OVER")]
+    [SerializeField]
+    private Sprite OVER;
+
+    const int MAXSCALE = 1;//画像の最大値
+    const int MINSCALE = 0;//画像の最小値
+
+    ////////////////////////////////////// 変数 //////////////////////////////////////
+
+    float fNowScale;//今のスケール
+    int iIntervalTime;//インターバルの時間
+
+    // Use this for initialization
+    void Start()
+    {
+        iIntervalTime = 0;//インターバルの時間を初期化
+        fNowScale = MINSCALE;//最小値を設定↓
+        GetComponent<RectTransform>().localScale = new Vector2(fNowScale, fNowScale);//自身のオブジェクトに当てる
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    //UIを表示させる
+    public bool ScaleChange(string imagetype, int time, float speed, bool flag)//使う画像 - インターバル - 速さ - 途中で終わるかのフラグ
+    {
+        switch (seuistatus)
+        {
+            case SEUI_STATUS._PANEL_START_:
+                switch (imagetype)//使う画像を変える
+                {
+                    case "START":
+                        this.gameObject.GetComponent<Image>().sprite = START;
+                        break;
+                    case "GOAL":
+                        this.gameObject.GetComponent<Image>().sprite = GOAL;
+                        break;
+                    case "OVER":
+                        this.gameObject.GetComponent<Image>().sprite = OVER;
+                        break;
+                }
+                break;
+
+            case SEUI_STATUS._PANEL_SCALEUP_://拡大させる
+                if (fNowScale < MAXSCALE)//最大値以下なら↓
+                {
+                    fNowScale += speed;//拡大
+                }
+                else if(!flag)//最大値以上になれば↓
+                {
+                    fNowScale = MAXSCALE;//サイズ修正
+                    seuistatus = SEUI_STATUS._PANEL_FIRST_INTERVAL_;//インターバルを挟む
+                }
+                else
+                {
+                    seuistatus = SEUI_STATUS._PANEL_START_;//インターバルの時間を経過すればスケールを縮小させる
+                    iIntervalTime = 0;//インターバルの時間の初期化
+                    return true;//終わる
+                }
+                break;
+            case SEUI_STATUS._PANEL_FIRST_INTERVAL_:
+
+                if (++iIntervalTime >= time)//インターバル中
+                {
+                    seuistatus = SEUI_STATUS._PANEL_SCALEDOWN_;//インターバルの時間を経過すればスケールを縮小させる
+                    iIntervalTime = 0;//インターバルの時間の初期化
+                }
+                break;
+
+            case SEUI_STATUS._PANEL_SCALEDOWN_://縮小させる
+                if (fNowScale > MAXSCALE)//最小値以下なら↓
+                {
+                    fNowScale -= speed;//縮小
+                }
+                else//最小値以上になれば↓
+                {
+                    fNowScale = MINSCALE;//サイズ修正
+                    seuistatus = SEUI_STATUS._PANEL_LAST_INTERVAL_;//インターバルを挟む
+                }
+                break;
+
+            case SEUI_STATUS._PANEL_LAST_INTERVAL_:
+
+                if (++iIntervalTime >= time)//インターバル中
+                {
+                    seuistatus = SEUI_STATUS._PANEL_START_;//インターバルの時間を経過すればスケールを縮小させる
+                    iIntervalTime = 0;//インターバルの時間の初期化
+                    return true;//終わり
+                }
+                break;
+        }
+        GetComponent<RectTransform>().localScale = new Vector2(fNowScale, fNowScale);//自身のオブジェクトに当てる
+        return false;
+    }
+}
